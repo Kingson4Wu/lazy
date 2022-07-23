dc36457bde603cee0b2ac6b7f1cad7e4635941cd
kingson4wu <kingson4wu@gmail.com>
2022-07-23 15:42:49 +0800

    gui

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/widget"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	//fmt.Println("Hello World")
+
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
++ go mod tidy
++ go mod vendor
+
++ https://github.com/fyne-io/fyne/
